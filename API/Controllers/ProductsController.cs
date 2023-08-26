907baa4ec0b8ad2785a121bad71f7e689fd20e41
Rachiket Ninawe <rninawe@axistechnical.co.in>
2023-08-26 22:27:19 +0530

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,24 +10,38 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context) 
+        private readonly IProductRepository _repo;
+
+        public ProductsController(IProductRepository repo) 
         {
-            _context = context;
+            _repo = repo;
 
         }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync(); 
-            return products;
+            var products = await _repo.GetProductsAsync(); 
+            return Ok((List<Product>)products);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            var product = await _context.Products.FindAsync(id);
+            var product = await _repo.GetProductByIdAsync(id);
             return product;
+        }
+        [HttpGet("types")]
+        public async Task<List<ProductType>> GetProductTypes()
+        {
+            var productTypes = await _repo.GetProductTypesAsync();
+            return (List<ProductType>)productTypes;
+        }
+
+        [HttpGet("brands")]
+        public async Task<List<ProductBrand>> GetProductBrands()
+        {
+            var productBrands = await _repo.GetProductBrandsAsync();
+            return (List<ProductBrand>)productBrands;
         }
     }
 }
